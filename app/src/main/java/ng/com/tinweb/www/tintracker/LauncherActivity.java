c62a32e2ff3c4eedf8ae433d06d1a1420054b716
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2015-10-29 14:05:17 +0100

    [Finishes #106930530] Implement splash screen for 3 seconds before application starts

---
+++ app/src/main/java/ng/com/tinweb/www/tintracker/LauncherActivity.java
@@ -0,0 +1,37 @@
+package ng.com.tinweb.www.tintracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int LAUNCHER_TIMEOUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(LauncherActivity.this, WelcomeActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, LAUNCHER_TIMEOUT);
+    }
+}
