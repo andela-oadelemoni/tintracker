aa645abba6c9be4648d2a295795cc3dfb87a0039
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@andela.com>
2015-11-03 13:36:16 +0100

    [Finishes #106931534] Implement setting changing feature

---
+++ app/src/main/java/ng/com/tinweb/www/tintracker/data/TrackerTimeSetting.java
@@ -0,0 +1,37 @@
+package ng.com.tinweb.www.tintracker.data;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import ng.com.tinweb.www.tintracker.R;
+
+/**
+ * Created by kamiye on 11/3/15.
+ */
+public class TrackerTimeSetting {
+
+    private static final int DEFAULT_TIME = 5;
+
+    private SharedPreferences sharedPref;
+    private Activity activity;
+
+    public TrackerTimeSetting(Activity activity) {
+        this.activity = activity;
+        createSharedPreference();
+    }
+
+    private void createSharedPreference() {
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(activity);
+    }
+
+    public void saveTimeSetting(int time) {
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(activity.getString(R.string.saved_high_score), time);
+        editor.apply();
+    }
+
+    public int getTimeSetting() {
+        return sharedPref.getInt(activity.getString(R.string.saved_high_score), DEFAULT_TIME);
+    }
+
+}
