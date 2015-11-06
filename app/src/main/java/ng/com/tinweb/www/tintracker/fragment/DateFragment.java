02db7e2f276c8466adea9b1edf028628c9b047c3
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@andela.com>
2015-11-06 16:29:00 +0100

    [#107512616] Implement Location history fragment to display location within main activity in a fragment

--- app/src/main/java/ng/com/tinweb/www/tintracker/fragment/DateFragment.java
+++ app/src/main/java/ng/com/tinweb/www/tintracker/fragment/DateFragment.java
@@ -2,6 +2,8 @@ package ng.com.tinweb.www.tintracker.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,11 @@ public class DateFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.history_date, container, false);
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.date_recycler_view);
+
+        // set the recyclerview layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
 
         return rootView;
     }
--- app/src/main/java/ng/com/tinweb/www/tintracker/fragment/LocationFragment.java
+++ app/src/main/java/ng/com/tinweb/www/tintracker/fragment/LocationFragment.java
@@ -3,11 +3,17 @@ package ng.com.tinweb.www.tintracker.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import ng.com.tinweb.www.tintracker.R;
+import ng.com.tinweb.www.tintracker.adapter.LocationHistoryAdapter;
+import ng.com.tinweb.www.tintracker.database.LocationData;
 
 /**
  * Created by kamiye on 11/6/15.
@@ -20,6 +26,17 @@ public class LocationFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.history_location, container, false);
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.location_recycler_view);
+
+        // set the recyclerview layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        LocationData location = new LocationData();
+        List<LocationData> locations = location.getLocations();
+
+        LocationHistoryAdapter adapter = new LocationHistoryAdapter(locations);
+        recyclerView.setAdapter(adapter);
 
         return rootView;
     }
