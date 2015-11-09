98c48bf56999a71ab68e5472373b22b00e79e85f
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2015-11-09 22:29:05 +0100

    [Finishes #107732482] Add application info for ease of use and understanding

---
+++ app/src/main/java/ng/com/tinweb/www/tintracker/fragment/InfoFragment.java
@@ -0,0 +1,22 @@
+package ng.com.tinweb.www.tintracker.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ng.com.tinweb.www.tintracker.R;
+
+/**
+ * Created by kamiye on 11/9/15.
+ */
+public class InfoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+}
